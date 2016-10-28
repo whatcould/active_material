19c3c3ab7dd7cde243756ec5831110fad8a3a960
David Reese <david@whatcould.com>
2016-10-27 22:10:00 -0400

    Switch to Open Sans
    Roboto is awful

--- lib/active_material/link_font_tag.rb
+++ lib/active_material/link_font_tag.rb
@@ -1,7 +1,7 @@
 module ActiveMaterial::LinkFontTag
   def build_active_admin_head
     within @head do
-      text_node(tag(:link, href: 'https://fonts.googleapis.com/css?family=Roboto:400,400i,500,700', rel: 'stylesheet'))
+      text_node(tag(:link, href: 'https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800', rel: 'stylesheet'))
       super
     end
   end
